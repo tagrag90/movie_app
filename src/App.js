3a3c4d5389f9b202d378ad25966d3de85f596b91
tagrag90 <gunjeepark21@gmail.com>
2020-06-03 03:03:41 +0900

    prop-type

--- src/App.js
+++ src/App.js
@@ -1,20 +1,69 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 // import Movie from './Movie';
-import Food from './Food';
+// import Food from './Food';
 
+function Food({ name, picture, rating }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name} />
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    id: 1,
+    name: 'kimchi',
+    image: 'https://ohyena.files.wordpress.com/2015/02/ec8aa4ed81aceba6b0ec83b7-2015-02-23-21-38-58.png',
+    rating: 4,
+  },
+  {
+    id: 2,
+    name: 'Samgyeopsal',
+    image: 'https://ohyena.files.wordpress.com/2015/02/ec8aa4ed81aceba6b0ec83b7-2015-02-23-21-38-58.png',
+    rating: 4.3,
+  },
+  {
+    id: 3,
+    name: 'Bibimbap',
+    image: 'https://ohyena.files.wordpress.com/2015/02/ec8aa4ed81aceba6b0ec83b7-2015-02-23-21-38-58.png',
+    rating: 3.4,
+  },
+  {
+    id: 4,
+    name: 'Doncasu',
+    image: 'https://ohyena.files.wordpress.com/2015/02/ec8aa4ed81aceba6b0ec83b7-2015-02-23-21-38-58.png',
+    rating: 4.9,
+  },
+  {
+    id: 5,
+    name: 'Kimbap',
+    image: 'https://ohyena.files.wordpress.com/2015/02/ec8aa4ed81aceba6b0ec83b7-2015-02-23-21-38-58.png',
+    rating: 5,
+  },
+
+];
 
 
 
 function App() {
   return (
     <div>
-      <h1>Hello!  React app.</h1>
-
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="samgiosal" />
-      <Food fav="chukumi" />
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
+      ))}
     </div>
   );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
+
+
 export default App;
--- src/Food.js
+++ src/Food.js
@@ -1,8 +1,36 @@
 import React from 'react';
 
-function Food({ fav }) {
-  // console.log(props);
-  return <h1>I like {fav}</h1>;
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h1>I like {name}</h1>
+      <img src={picture} />
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name: 'kimchi',
+    image: 'https://www.google.com/url?sa=i&url=http%3A%2F%2Fjnmall.kr%2Fproduct%2F%25EC%2582%25B0%25EB%2593%25A4%25EB%259E%2598-%25EA%25B9%2580%25EC%259E%25A5-%25EB%25B0%25B0%25EC%25B6%2594%25EA%25B9%2580%25EC%25B9%2598-10kg%2F4986%2F&psig=AOvVaw12zG8JwB_F29RxwQpnEjqu&ust=1591202944665000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLjSpYfL4-kCFQAAAAAdAAAAABAD',
+  },
+  {
+    name: 'Samgyeopsal',
+    image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fporkboard.han-don.com%2Fporkboard%2Fbbs%2Findex.php%3Fmode%3Dview%26id%3D470%26board%3Dporkboard_121&psig=AOvVaw2cET-xMIvWWTlhhMSmiDH7&ust=1591202989019000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOD0-JnL4-kCFQAAAAAdAAAAABAD',
+  },
+  {
+    name: 'Bibimbap',
+    image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.10000recipe.com%2Frecipe%2F6896992&psig=AOvVaw1QfF7tWksi4kXXzYX8idH7&ust=1591203012707000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKDgxqXL4-kCFQAAAAAdAAAAABAD',
+  },
+  {
+    name: 'Doncasu',
+    image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fgeniusjw.com%2F1864&psig=AOvVaw0F-OHsWrlkJ8ORpNlVxZhu&ust=1591203035765000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKCbvLDL4-kCFQAAAAAdAAAAABAT',
+  },
+  {
+    name: 'Kimbap',
+    image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fhomecuisine.co.kr%2Fhc10%2F83001&psig=AOvVaw1WCZyE6w1CKQ5t7EbCKUBG&ust=1591203074686000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMDFpsTL4-kCFQAAAAAdAAAAABAD',
+  },
+
+];
 
 export default Food;
